232ffeb913161cb5fef4eff35c7d20bc3b3a75f4
conglt <conglt1712@gmail.com>
2020-10-28 10:39:56 +0700

    fix: get status

--- contracts/migrations/2_next.js
+++ contracts/migrations/2_next.js
@@ -1,9 +1,8 @@
-const PhoneToken = artifacts.require('./PhoneToken.sol');
 const PhoneAirdrop = artifacts.require('./PhoneAirdrop.sol');
 
 require('dotenv').config();
 
 module.exports = async function (deployer) {
-  await deployer.deploy(PhoneToken);
-  await deployer.deploy(PhoneAirdrop, PhoneToken.address);
+  const phoneAddress = process.env.PHONE_ADDRESS;
+  await deployer.deploy(PhoneAirdrop, phoneAddress);
 };
